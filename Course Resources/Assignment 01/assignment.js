1e15c87ef4e3e2ad59f45a10c0c1fdd439d6a884
Adam Bundschuh <bundschuh.adam@gmail.com>
2021-04-08 22:36:57 -0400

    First assignment.
    Created several variables, one with an intial value (userNumber) and
    another that held no value initially (result)
    
    Gave value to result based on userNumber, then
    manilpulated the value several times, and finally
    output both values via the alert function.

---
+++ Course Resources/Assignment 01/assignment.js
@@ -0,0 +1,11 @@
+let userNumber = 20; // optional const userNumber = 20;
+let result;
+
+result = 18 + userNumber;
+
+result = result - 7;
+result = result * 3;
+result = result / 2;
+
+alert (result);
+alert (userNumber);
