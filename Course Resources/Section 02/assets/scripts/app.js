8a235c9ede50e49410afc4c23bb0407844ed2542
Adam Bundschuh <bundschuh.adam@gmail.com>
2021-04-08 21:38:01 -0400

    Differences between integer and string variables

--- Course Resources/Section 02/assets/scripts/app.js
+++ Course Resources/Section 02/assets/scripts/app.js
@@ -1,1 +1,7 @@
-alert('This Works!');
+let currentResult = 0;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = '(0 + 10) * 3 / 2 - 1';
+
+outputResult(currentResult, '');
